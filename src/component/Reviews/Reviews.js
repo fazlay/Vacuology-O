590b06601ff26497d25541933385923c8675a6e5
Fazlay Rabbi <rfazlay21@gmail.com>
2021-11-12 14:48:54 +0600

    dshbord changes

--- src/component/Reviews/Reviews.js
+++ src/component/Reviews/Reviews.js
@@ -1,9 +1,18 @@
 import React from 'react';
+import './Review.css'
 
 const Reviews = () => {
   return (
     <div>
-      <h2>This is Review </h2>
+     <div class="tm-content">
+<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. In ultrices, elit sed faucibus pharetra, diam mauris bibendum orci, sit amet ullamcorper purus dui sit amet augue. Donec aliquet diam ut neque mattis, eu tristique sem rutrum.</p>
+</div>
+<div class="tm-pic">
+<div class="photo"><img src="https://img.webnots.com/2015/08/Testimonial3.jpg"/>
+</div>
+<p>Diana Mary</p>
+<p>Important person, some Company</p>
+</div>
     </div>
   );
 };
