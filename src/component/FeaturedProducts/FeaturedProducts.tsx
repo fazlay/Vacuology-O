8b1caa0a54234c2de570e4c68799804a979e6b8d
fazlay <rfazlay21@gmail.com>
2023-05-12 21:21:48 +0600

    protipdone

---
+++ src/component/FeaturedProducts/FeaturedProducts.tsx
@@ -0,0 +1,57 @@
+import { Container, Divider, Grid, Skeleton, Typography } from "@mui/material";
+import { useEffect, useState } from "react";
+import CardSkeleton from "./CardSkeleton";
+import SingleProduct from "./SingleProduct/SingleProduct";
+import { apiMethod } from "../../config/apiMethod";
+import ProTipCard from "../common/ProTiopCard";
+
+const FeaturedProducts = () => {
+  const [products, setProducts] = useState<any>([]);
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  useEffect(() => {
+    apiMethod.get("/products").then((res) => {
+      const manipulateData = res.data.splice(4, 0, null);
+      // console.log({ manipulateData });
+      setProducts(res.data);
+      setIsLoading(true);
+    });
+  }, []);
+
+  return (
+    <Container>
+      <Typography
+        gutterBottom
+        variant="h3"
+        color="text.secondary"
+        component="div"
+        sx={{ fontWeight: "bold", pt: 5, textAlign: "center" }}
+      >
+        Our Available Products
+      </Typography>
+      <Divider variant="middle" />
+      <>{console.log(products)}</>
+      <Grid container spacing={2}>
+        {!isLoading ? (
+          <Skeleton>
+            <CardSkeleton></CardSkeleton>
+          </Skeleton>
+        ) : (
+          <>
+            {products.slice(0, 9).map((product) => {
+              return !!product ? (
+                <SingleProduct
+                  product={product}
+                  key={product._id}
+                ></SingleProduct>
+              ) : (
+                <ProTipCard />
+              );
+            })}
+          </>
+        )}
+      </Grid>
+    </Container>
+  );
+};
+
+export default FeaturedProducts;
